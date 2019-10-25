69559a5c2768bb27462101276ff9ee1099fad784
James Groves <triskept@gmail.com>
2019-10-25 16:48:02 -0400

    a little farther, blocker continuing game

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,12 @@ import blackjack from "blackjack-dealer-logic"
 
 export default () =>{
     const game = blackjack.singleDeckGame
-    const gameIsRunning = true
+    let gameIsRunning = false
     const startButton = document.getElementById("btn-play")
+    const hitButton = document.getElementById("btn-hit")
+    const standButton = document.getElementById("btn-stand")
+    const doubleButton = document.getElementById("btn-double")
+
    
     alert("Welcome to Blackjack")
     alert("Press play to begin")
@@ -12,25 +16,50 @@ export default () =>{
 
     startButton.onclick = function(){
         document.getElementById("text")
-        text.innerText = `The amount of money in your wallet: ${game.getUserChips()}`
+        // text.innerText = `The amount of money in your wallet: ${game.getUserChips()}`
         const wager = window.prompt("Enter your wager: ");
         game.receiveAnte(wager)
-        document.write(`Your wager is: ${wager}`)
-        game.deal()
-        document.write(`Dealer is showing: ${game.getDealerCardUp()}`)
-        document.write(`Your current hand: ${game.getUserHandValue()}`)
 
+        setTimeout(function() {const wagerDisplay = document.getElementById('text')
+        wagerDisplay.innerText =`Your wager is: ${wager}`}, 2000)
+
+        game.deal()
+
+        setTimeout(function() {const cardDisplay = document.getElementById("dealerCards")
+        cardDisplay.innerHTML = `Dealer is showing: ${game.getDealerCardUp()}`},2000)
+
+        const yourcardDisplay = document.getElementById("yourCards")
+        yourcardDisplay.innerHTML = `You are showing: ${game.getUserHandValue()}`
+        
+        setTimeout(function() {const ques1 = document.getElementById("Q1")
+        ques1.innerHTML = `Whats your move?: `},5000)        
+    }
+
+    hitButton.onclick = function (){
+        game.hitUser();
+        game.evaluateUser();
+        const yourcardDisplay2 = document.getElementById("yourCard2")
+        yourcardDisplay2.innerHTML = `You are showing: ${game.getUserHandValue()}`
     }
     
+    standButton.onclick = function(){
+        game.standUser();
+        game.evaluateUser();
+        const yourcardDisplay3 = document.getElementById("yourCard3")
+        yourcardDisplay3.innerHTML = `You are showing: ${game.getUserHandValue()}`
+    }
+
+    game.settleDealerHand()
+
+    const cardDisplay = document.getElementById("dealerCards")
+    cardDisplay.innerHTML = `Dealer is showing: ${game.getDealerHandValue()}`
+
+
+
+    // doubleButton.onclick = function(){
+    //     game.doubleUser();
+    //     game.evaluateUser();
+    //     const dblWager = document.getElementById("dblwager")
+    //     dblWager.innerText =`Your wager is: ${wager}`
+    // }
 }
-
-// const gameIsRunning = true;
-
-
-// while(gameIsRunning) {
-// p();
-// document.body.textContent = `The amount of money in your wallet: ${singleDeckGame.getUserChips()}`;
-// const userWager = Number(ask("Please enter an amount to wager: "));
-// singleDeckGame.recieveAnte(userWager);
-// p();
-// }
